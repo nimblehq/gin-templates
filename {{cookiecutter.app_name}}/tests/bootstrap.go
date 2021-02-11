7b940b49b51cfd0110f18e117e4717b085381ee1
Gut <thiramate@nimblehq.co>
2021-02-11 17:42:11 +0700

    Add test bootstrap

---
+++ {{cookiecutter.app_name}}/tests/bootstrap.go
@@ -0,0 +1,15 @@
+package tests
+
+import (
+	"github.com/nimblehq/xxxx/bootstrap"
+
+	"github.com/gin-gonic/gin"
+)
+
+var Router *gin.Engine
+
+func SetupTestEnvironment() {
+	gin.SetMode(gin.TestMode)
+
+	Router = bootstrap.SetupRouter()
+}
