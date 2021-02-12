ea5c148bae0b0076cfd116d3ef61a8c6ccc669ce
Gut <thiramate@nimblehq.co>
2021-02-12 18:19:52 +0700

    Update cookiecutter name for development purpose

--- {{cookiecutter.app_name}}/cmd/api/main.go
+++ {{cookiecutter.app_name}}/cmd/api/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"log"
 
-	"github.com/nimblehq/{{cookiecutter.app_name}}/bootstrap"
+	"github.com/nimblehq/xxxx/bootstrap"
 )
 
 func main() {
--- {{cookiecutter.app_name}}/test/test.go
+++ {{cookiecutter.app_name}}/test/test.go
@@ -1,7 +1,7 @@
 package test
 
 import (
-	"github.com/nimblehq/{{cookiecutter.app_name}}/bootstrap"
+	"github.com/nimblehq/xxxx/bootstrap"
 
 	"github.com/gin-gonic/gin"
 )
