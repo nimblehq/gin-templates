504622e5251ad7ee6453bfb2fc842fb9b2704b34
Gut <thiramate@nimblehq.co>
2021-02-15 12:22:32 +0700

    Remove loadconfig from router

--- {{cookiecutter.app_name}}/bootstrap/router.go
+++ {{cookiecutter.app_name}}/bootstrap/router.go
@@ -1,23 +1,12 @@
 package bootstrap
 
 import (
-	"log"
-
 	apiv1router "github.com/nimblehq/xxxx/lib/api/v1/routers"
-	"github.com/spf13/viper"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
-	viper.SetConfigName("config")
-	viper.SetConfigType("conf")
-	viper.AddConfigPath("./config")
-	// viper.AutomaticEnv()
-	_ = viper.ReadInConfig()
-	log.Printf("=============== %v", gin.Mode())
-	log.Printf("=============== %v", viper.GetString(gin.Mode()+".dbURL"))
-
 	r := gin.Default()
 
 	apiv1router.ComebineRoutes(r)
