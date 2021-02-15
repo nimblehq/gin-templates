bb16f19996a6fe8f6ef87e362dea0e942c89861a
Gut <thiramate@nimblehq.co>
2021-02-15 17:16:58 +0700

    Add common get value from config functions

--- {{cookiecutter.app_name}}/helpers/config.go
+++ {{cookiecutter.app_name}}/helpers/config.go
@@ -13,6 +13,18 @@ func GetConfigPrefix() string {
 	return gin.Mode() + "."
 }
 
-func ReadConfig(key string) string {
+func GetBoolConfig(key string) bool {
+	return viper.GetBool(GetConfigPrefix() + key)
+}
+
+func GetFloatConfig(key string) float64 {
+	return viper.GetFloat64(GetConfigPrefix() + key)
+}
+
+func GetIntConfig(key string) int {
+	return viper.GetInt(GetConfigPrefix() + key)
+}
+
+func GetStringConfig(key string) string {
 	return viper.GetString(GetConfigPrefix() + key)
 }
