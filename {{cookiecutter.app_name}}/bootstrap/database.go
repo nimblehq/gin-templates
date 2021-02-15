499c201bbfec24f95dada9fad2033ee6b36c5d5d
Gut <thiramate@nimblehq.co>
2021-02-15 17:16:42 +0700

    Update set viper database config

--- {{cookiecutter.app_name}}/bootstrap/database.go
+++ {{cookiecutter.app_name}}/bootstrap/database.go
@@ -10,20 +10,13 @@ import (
 	"gorm.io/gorm"
 )
 
-type User struct {
-	gorm.Model
-	Email    string `gorm:"unique;notNull;index"`
-	Password string `gorm:"notNull"`
-}
-
 // Init database and set to viper config
 func InitDatabase() {
-	db, err := gorm.Open(postgres.Open(helpers.ReadConfig("database_url")), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(helpers.GetStringConfig("database_url")), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database: ", err)
 	} else {
+		viper.Set("database", db)
 		log.Println("Database connected successfully.")
 	}
-
-	viper.Set("database", db)
 }
