7fdc56c96c9a2bf192b3078ee98f0a36cfb711d9
Gut <thiramate@nimblehq.co>
2021-02-15 17:17:25 +0700

    Update get db function

--- {{cookiecutter.app_name}}/helpers/database.go
+++ {{cookiecutter.app_name}}/helpers/database.go
@@ -1,11 +1,18 @@
 package helpers
 
 import (
+	"errors"
+
 	"github.com/spf13/viper"
 	"gorm.io/gorm"
 )
 
 // Get database connection from viper config
-func GetDBConnection() *gorm.DB {
-	return viper.Get("database").(*gorm.DB)
+func GetDBConnection() (*gorm.DB, error) {
+	dbConnection, ok := viper.Get("database").(*gorm.DB)
+	if !ok {
+		return nil, errors.New("Failed to get database connection: connection is not type of *gorm.DB")
+	}
+
+	return dbConnection, nil
 }
