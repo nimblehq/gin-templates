1d8049f312dff71e188bf06800c3915a19dc4181
Gut <thiramate@nimblehq.co>
2021-02-18 12:19:08 +0700

    Add unit test for template generation

---
+++ tests/create_templates_test.go
@@ -0,0 +1,100 @@
+package tests_test
+
+import (
+	"os"
+	"strings"
+
+	"github.com/nimblehq/gin-templates/tests"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = BeforeEach(func() {
+	tests.ChangeDirectory(ProjectGeneratedPath + "/test-gin-templates")
+})
+
+var _ = Describe("Create", func() {
+	Context("given current directory", func() {
+		It("contains project name", func() {
+			dir, err := os.Getwd()
+			if err != nil {
+				Fail("Failed to get current directory: " + err.Error())
+			}
+
+			isContainProjectName := strings.Contains(dir, "gin-templates/tests/test-gin-templates")
+
+			Expect(isContainProjectName).To(BeTrue())
+		})
+	})
+
+	Context("given go.mod", func() {
+		It("contains project name at module name", func() {
+			content := tests.ReadFile("go.mod")
+
+			isContainProjectName := strings.Contains(content, "module github.com/nimblehq/test-gin-templates")
+
+			Expect(isContainProjectName).To(BeTrue())
+		})
+	})
+
+	Context("given bootstrap/router.go", func() {
+		It("contains project name at api v1 routers import", func() {
+			content := tests.ReadFile("bootstrap/router.go")
+
+			isContainProjectName := strings.Contains(content, `apiv1router "github.com/nimblehq/test-gin-templates/lib/api/v1/routers"`)
+
+			Expect(isContainProjectName).To(BeTrue())
+		})
+	})
+
+	Context("given cmd/api/main.go", func() {
+		It("contains project name at bootstrap import", func() {
+			content := tests.ReadFile("cmd/api/main.go")
+
+			isContainProjectName := strings.Contains(content, `"github.com/nimblehq/test-gin-templates/bootstrap"`)
+
+			Expect(isContainProjectName).To(BeTrue())
+		})
+	})
+
+	Context("given lib/api/v1/controllers/controllers_suite_test.go", func() {
+		It("contains project name at test import", func() {
+			content := tests.ReadFile("lib/api/v1/controllers/controllers_suite_test.go")
+
+			isContainProjectName := strings.Contains(content, `"github.com/nimblehq/test-gin-templates/test"`)
+
+			Expect(isContainProjectName).To(BeTrue())
+		})
+	})
+
+	Context("given lib/api/v1/controllers/health_test.go", func() {
+		It("contains project name at test import", func() {
+			content := tests.ReadFile("lib/api/v1/controllers/health_test.go")
+
+			isContainProjectName := strings.Contains(content, `"github.com/nimblehq/test-gin-templates/test"`)
+
+			Expect(isContainProjectName).To(BeTrue())
+		})
+	})
+
+	Context("given lib/api/v1/routers/router.go", func() {
+		It("contains project name at api v1 controllers import", func() {
+			content := tests.ReadFile("lib/api/v1/routers/router.go")
+
+			isContainProjectName := strings.Contains(content, `"github.com/nimblehq/test-gin-templates/lib/api/v1/controllers"`)
+
+			Expect(isContainProjectName).To(BeTrue())
+		})
+	})
+
+	Context("given test/test.go", func() {
+		It("contains project name at bootstrap import", func() {
+			content := tests.ReadFile("test/test.go")
+
+			isContainProjectName := strings.Contains(content, `"github.com/nimblehq/test-gin-templates/bootstrap"`)
+
+			Expect(isContainProjectName).To(BeTrue())
+		})
+	})
+})
---
+++ tests/tests_suite_test.go
@@ -0,0 +1,57 @@
+package tests_test
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var ProjectGeneratedPath string
+
+func TestTemplate(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Controllers Suite")
+}
+
+var _ = BeforeSuite(func() {
+	input := []byte("test-gin-templates")
+	r, w, err := os.Pipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = w.Write(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Close()
+
+	stdin := r
+
+	shCmd := exec.Command("cookiecutter", "..")
+	shCmd.Stdout = os.Stdout
+	shCmd.Stdin = stdin
+
+	err = shCmd.Run()
+
+	if err != nil {
+		log.Fatal("Gin project created unsuccessfully: ", err.Error())
+	} else {
+		log.Print("Gin project created successfully.")
+	}
+
+	currentPath, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ProjectGeneratedPath = currentPath
+})
+
+var _ = AfterSuite(func() {
+	os.RemoveAll(ProjectGeneratedPath + "/test-gin-templates")
+})
