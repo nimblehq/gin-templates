9d017c79285f6c9dbbd9e13861be4c7f309d3304
Gut <thiramate@nimblehq.co>
2021-02-18 17:42:09 +0700

    Add more test cases

--- tests/template/create_template_test.go
+++ tests/template/create_template_test.go
@@ -28,11 +28,57 @@ var _ = Describe("Create template", func() {
 		})
 	})
 
+	Context("given docker-compose.dev.yml", func() {
+		It("contains project name at container_name", func() {
+			content := tests.ReadFile("docker-compose.dev.yml")
+
+			isContainProjectName := strings.Contains(content, "container_name: test-gin-templates_db")
+
+			Expect(isContainProjectName).To(BeTrue())
+		})
+
+		It("contains project name at postgres db env", func() {
+			content := tests.ReadFile("docker-compose.dev.yml")
+
+			isContainProjectName := strings.Contains(content, "- POSTGRES_DB=test-gin-templates_development")
+
+			Expect(isContainProjectName).To(BeTrue())
+		})
+	})
+
+	Context("given docker-compose.test.yml", func() {
+		It("contains project name at container_name", func() {
+			content := tests.ReadFile("docker-compose.test.yml")
+
+			isContainProjectName := strings.Contains(content, "container_name: test-gin-templates_db_test")
+
+			Expect(isContainProjectName).To(BeTrue())
+		})
+
+		It("contains project name at postgres db env", func() {
+			content := tests.ReadFile("docker-compose.test.yml")
+
+			isContainProjectName := strings.Contains(content, "- POSTGRES_DB=test-gin-templates_test")
+
+			Expect(isContainProjectName).To(BeTrue())
+		})
+	})
+
 	Context("given go.mod", func() {
 		It("contains project name at module name", func() {
 			content := tests.ReadFile("go.mod")
 
 			isContainProjectName := strings.Contains(content, "module github.com/nimblehq/test-gin-templates")
+
+			Expect(isContainProjectName).To(BeTrue())
+		})
+	})
+
+	Context("given bootstrap/database.go", func() {
+		It("contains project name at helpers import", func() {
+			content := tests.ReadFile("bootstrap/database.go")
+
+			isContainProjectName := strings.Contains(content, `"github.com/nimblehq/test-gin-templates/helpers"`)
 
 			Expect(isContainProjectName).To(BeTrue())
 		})
@@ -53,6 +99,52 @@ var _ = Describe("Create template", func() {
 			content := tests.ReadFile("cmd/api/main.go")
 
 			isContainProjectName := strings.Contains(content, `"github.com/nimblehq/test-gin-templates/bootstrap"`)
+
+			Expect(isContainProjectName).To(BeTrue())
+		})
+	})
+
+	Context("given config/app.toml", func() {
+		It("contains project name at app_name", func() {
+			content := tests.ReadFile("config/app.toml")
+
+			isContainProjectName := strings.Contains(content, `app_name = "test-gin-templates"`)
+
+			Expect(isContainProjectName).To(BeTrue())
+		})
+
+		It("contains project name at debug db name", func() {
+			content := tests.ReadFile("config/app.toml")
+
+			isContainProjectName := strings.Contains(content, `db_name = "test-gin-templates_development"`)
+
+			Expect(isContainProjectName).To(BeTrue())
+		})
+
+		It("contains project name at test db name", func() {
+			content := tests.ReadFile("config/app.toml")
+
+			isContainProjectName := strings.Contains(content, `db_name = "test-gin-templates_test"`)
+
+			Expect(isContainProjectName).To(BeTrue())
+		})
+	})
+
+	Context("given helpers/config_test.go", func() {
+		It("contains project name at helpers import", func() {
+			content := tests.ReadFile("helpers/config_test.go")
+
+			isContainProjectName := strings.Contains(content, `"github.com/nimblehq/test-gin-templates/helpers"`)
+
+			Expect(isContainProjectName).To(BeTrue())
+		})
+	})
+
+	Context("given helpers/helpers_suite_test.go", func() {
+		It("contains project name at test import", func() {
+			content := tests.ReadFile("helpers/helpers_suite_test.go")
+
+			isContainProjectName := strings.Contains(content, `"github.com/nimblehq/test-gin-templates/test"`)
 
 			Expect(isContainProjectName).To(BeTrue())
 		})
