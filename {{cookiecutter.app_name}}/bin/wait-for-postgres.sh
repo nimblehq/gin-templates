7b1723bf3f3344c9f529311ed6a83adfd8945e28
Gut <thiramate@nimblehq.co>
2021-02-24 14:20:05 +0700

    Update wait for pg

--- {{cookiecutter.app_name}}/bin/wait-for-postgres.sh
+++ {{cookiecutter.app_name}}/bin/wait-for-postgres.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-set -x
+set -e
 
-until psql $DATABASE_URL -c "select 1" > /dev/null 2>&1; do
+until psql $DATABASE_URL -c "\q" > /dev/null 2>&1; do
+  >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
