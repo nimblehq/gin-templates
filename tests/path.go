5262218f66eb794f6439f3eb3c1cd2b4c28f7942
Gut <thiramate@nimblehq.co>
2021-06-18 14:58:39 +0700

    Able to pass parameter to function

--- tests/path.go
+++ tests/path.go
@@ -6,6 +6,15 @@ import (
 	. "github.com/onsi/ginkgo"
 )
 
+func GetCurrentDirectory() string {
+	currentPath, err := os.Getwd()
+	if err != nil {
+		Fail("Failed to get current directory: " + err.Error())
+	}
+
+	return currentPath
+}
+
 func ChangeDirectory(dir string) {
 	err := os.Chdir(dir)
 	if err != nil {
--- tests/template.go
+++ tests/template.go
@@ -3,47 +3,12 @@ package tests
 import (
 	"io"
 	"log"
-	"os"
 	"os/exec"
 
 	. "github.com/onsi/ginkgo"
 )
 
-func DownloadGinTemplate() {
-	shCmd := exec.Command("go", "get", "github.com/nimblehq/gin-templates")
-	shCmd.Stdout = os.Stdout
-	err := shCmd.Run()
-	if err != nil {
-		Fail("Failed to get gin-templates: " + err.Error())
-	} else {
-		log.Println("Gin-templates downloaded successfully.")
-	}
-}
-
-func BuildGinTemplate() {
-	shCmd := exec.Command("go", "build", "-o", os.Getenv("GOPATH")+"/bin/nimble-gin", "github.com/nimblehq/gin-templates")
-	shCmd.Stdout = os.Stdout
-	err := shCmd.Run()
-	if err != nil {
-		Fail("Failed to build gin-templates: " + err.Error())
-	} else {
-		log.Println("Gin-templates built successfully.")
-	}
-}
-
-func RemoveCookiecuttersCache() {
-	shCmd := exec.Command("rm", "-rf", os.Getenv("HOME")+"/.cookiecutters/gin-templates")
-	shCmd.Stdout = os.Stdout
-
-	err := shCmd.Run()
-	if err != nil {
-		Fail("Failed to remove cookiecutters cache: " + err.Error())
-	} else {
-		log.Println("Cookiecutters cache removed successfully.")
-	}
-}
-
-func CreateProjectFromGinTemplate(input string) string {
+func CreateProjectFromGinTemplate(input string, templateGeneratedPath string) {
 	shCmd := exec.Command("cookiecutter", "../")
 
 	stdin, err := shCmd.StdinPipe()
@@ -66,10 +31,5 @@ func CreateProjectFromGinTemplate(input string) string {
 		log.Println("Template created successfully.")
 	}
 
-	currentPath, err := os.Getwd()
-	if err != nil {
-		Fail("Failed to get current directory: " + err.Error())
-	}
-
-	return currentPath
+	ChangeDirectory(templateGeneratedPath + "/test-gin-templates")
 }
