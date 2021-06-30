5ad6b674b275b82a3432b642c32d698737148f5f
Hoang Mirs <hoang.mirs@gmail.com>
2021-07-01 00:21:07 +0700

    [50] Add web variant

--- {{cookiecutter.app_name}}/bootstrap/router.go
+++ {{cookiecutter.app_name}}/bootstrap/router.go
@@ -2,6 +2,7 @@ package bootstrap
 
 import (
 	apiv1router "github.com/nimblehq/{{cookiecutter.app_name}}/lib/api/v1/routers"
+	{% if cookiecutter.web_variant == "y" %}webrouter "github.com/nimblehq/{{cookiecutter.app_name}}/lib/web/routers"{% endif %}
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,6 +11,7 @@ func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
 	apiv1router.ComebineRoutes(r)
+	webrouter.ComebineRoutes(r)
 
 	return r
 }
