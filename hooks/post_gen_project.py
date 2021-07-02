0d8e2b6261e2163c39d68f02f18168da76564bd7
Xavier MALPARTY <xavier@nimblehq.co>
2021-07-02 18:05:56 +0700

    .#50 Add post_gen_hook to facilitate file deletion for future development

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,14 +4,19 @@ import shutil
 # Get the root project directory
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
-# Removes log helper folder
-def remove_logrus_files():
+def remove_files(path):
+    """
+    Removes files in path
+    """
     shutil.rmtree(os.path.join(
-        PROJECT_DIRECTORY, "helpers/log"
+        PROJECT_DIRECTORY, path
     ))
 
 # Print log with color
 def print_log(message):
+    """
+    Print log with color
+    """
     CYELLOW = '\33[33m'  # YELLOW color
     CEND = '\033[0m'  # END color
     print(CYELLOW + message + CEND)
@@ -20,7 +25,7 @@ def print_log(message):
 # Remove logrus add-on if not seleted
 if '{{ cookiecutter.use_logrus }}'.lower() == 'no':
     print_log('Removing logrus add-on')
-    remove_logrus_files()
+    remove_files("helpers/log")
 
 # Download the missing dependencies
 print_log('Downloading dependencies')
@@ -29,3 +34,9 @@ os.system("go mod tidy")
 # Format code
 print_log('Formating code')
 os.system("go fmt ./...")
+
+# # Use this snippet to remove files/folders based on
+# # cookiecutter.variables (cf pre_gen_project.py hook)
+# if '{{ cookiecutter.web_variant }}' == 'n':
+#     print_log('Removing web variant related files')
+#     remove_files('/path/to/remove')
