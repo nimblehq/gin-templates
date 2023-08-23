2fc0d99fa049d9b38bc0d0815f5544b462bd5327
Su Ho <su.v@nimblehq.co>
2023-08-23 11:32:44 +0700

    [#105] Improve logic for api variant

--- {{cookiecutter.app_name}}/bootstrap/router.go
+++ {{cookiecutter.app_name}}/bootstrap/router.go
@@ -1,22 +1,22 @@
 package bootstrap
 
 import (
-	{%- if cookiecutter.use_openapi == "yes" -%}apidocsrouter "github.com/nimblehq/{{cookiecutter.app_name}}/lib/api/docs/routers"{%- endif %}
+	{%- if cookiecutter._api_variant == "yes" -%}apidocsrouter "github.com/nimblehq/{{cookiecutter.app_name}}/lib/api/docs/routers"{%- endif %}
 	apiv1router "github.com/nimblehq/{{cookiecutter.app_name}}/lib/api/v1/routers"
 	{% if cookiecutter._web_variant == "yes" %}webrouter "github.com/nimblehq/{{cookiecutter.app_name}}/lib/web/routers"
 	{% endif %}
 
-	{%- if cookiecutter.use_openapi == "yes" -%}"github.com/gin-gonic/contrib/static"{%- endif %}
+	{%- if cookiecutter._api_variant == "yes" -%}"github.com/gin-gonic/contrib/static"{%- endif %}
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
-	{% if cookiecutter.use_openapi == "yes" -%}r.Use(static.Serve("/", static.LocalFile("./public", true))){%- endif %}
+	{% if cookiecutter._api_variant == "yes" -%}r.Use(static.Serve("/", static.LocalFile("./public", true))){%- endif %}
 
 	apiv1router.ComebineRoutes(r)
-	{% if cookiecutter.use_openapi == "yes" -%}apidocsrouter.ComebineRoutes(r){%- endif %}
+	{% if cookiecutter._api_variant == "yes" -%}apidocsrouter.ComebineRoutes(r){%- endif %}
 	{% if cookiecutter._web_variant == "yes" %}webrouter.ComebineRoutes(r)
 	{% endif %}
 	return r
